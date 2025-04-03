9a9891034b0b9c6f470fd37111ca920c3f0b9784
Eduard Dumitrescul <dumitresculeduard@gmail.com>
2025-04-03 10:00:58 +0300

    added course materials and template

---
+++ Template/include/Shader.h
@@ -0,0 +1,31 @@
+﻿#ifndef SHADER_H
+#define SHADER_H
+
+#include "glm/glm.hpp"	
+#include "GL/glew.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+
+class Shader {
+    // the program ID
+    unsigned int ID;
+public:
+    // constructor reads and builds the shader
+    Shader(const char* vertexPath, const char* fragmentPath);
+    // use/activate the shader
+    void use();
+    // utility uniform functions
+    void setBool(const std::string& name, bool value) const;
+    void setInt(const std::string& name, int value) const;
+    void setFloat(const std::string& name, float value) const;
+    void setVec4(const std::string& name, glm::vec4 value) const;
+    void setVec3(const std::string& name, glm::vec3 value) const;
+    void setMat4(const std::string& name, glm::mat4& value) const;
+
+    ~Shader();
+};
+
+#endif
